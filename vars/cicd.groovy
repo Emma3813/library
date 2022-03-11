8011476e1a342ef5ebe9886e8978977ec40f0e08
Emma3813 <97907103+Emma3813@users.noreply.github.com>
2022-03-11 17:07:02 -0500

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,9 +8,14 @@ def newmaven()
   sh 'mvn package'
 }
 
-def newdeploy(ip,appname)
+def newgit(ip,appname)
 {
   deploy adapters: [tomcat9(credentialsId: 'b7afcd11-dc4e-4f4e-a8af-990bd5353677', path: '', url: "${ip}")], contextPath: "${appname}", war: '**/*.war'
+}
+
+def newselenium(path)
+{
+  sh 'java -jar "${path}/testing.jar'
 }
 
 
